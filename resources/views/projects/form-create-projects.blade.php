2384772df4c7ba5729d617eca7cceda40c2e24c2
eduardo santos silva <eduardo.silva@kvminformatica.com.br>
2024-08-17 17:28:22 -0300

    Criar project

---
+++ resources/views/projects/form-create-projects.blade.php
@@ -0,0 +1,59 @@
+<section class="space-y-6">
+    <header>
+        <p class="mt-1 text-sm text-gray-600">
+            {{ __('Todos os seus projetos estão aqui.') }}
+        </p>
+    </header>
+
+    <x-primary-button
+        x-data=""
+        x-on:click.prevent="$dispatch('open-modal', 'create-project')"
+    >{{ __('Criar novo projeto') }}</x-primary-button>
+
+    <x-modal name="create-project" :show="$errors->userDeletion->isNotEmpty()" focusable>
+        <form method="post" action="{{ route('create_project') }}" class="p-6">
+            @csrf
+            @method('post')
+
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ __('Criando novo projeto') }}
+            </h2>
+
+
+
+            <div class="mt-6">
+                <x-input-label for="password" value="{{ __('Titulo') }}" class="sr-only" />
+
+                <x-text-input
+                    id="titulo"
+                    name="titulo"
+                    type="text"
+                    class="mt-1 block w-3/4"
+                    placeholder="{{ __('Titulo do projeto') }}"
+                />
+                <x-input-error :messages="$errors->userDeletion->get('titulo')" class="mt-2" />
+
+
+
+                <textarea id="message" rows="4"
+                class="mt-1 block w-3/4" placeholder="Descrição do projeto"
+                ></textarea>
+
+                <label for="">   {{ __('Data encerramento') }}</label>
+                <input type="date" name="date_encerramento" id="date_encerramento">
+
+
+            </div>
+
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-primary-button class="ms-3">
+                    {{ __('Criar projeto') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </x-modal>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('project')->group(function ()
+    {
+        Route::post('create/', [ProjectsController::class, 'create'])->name('create_project');
+    });
 });
 
 require __DIR__.'/auth.php';
