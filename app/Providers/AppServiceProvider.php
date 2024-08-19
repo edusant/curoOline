b78019b012654afd7db94a151451aa4702cbe580
eduardo santos silva <eduardo.silva@kvminformatica.com.br>
2024-08-19 00:07:15 -0300

    Create polecy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Projects;
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Gate::define('update-porject', function (User $user, $projectId) {
+            return Projects::find($projectId)->user_id === $user->id
+                        ? Response::allow()
+                        : Response::deny('');
+        });
     }
 }
