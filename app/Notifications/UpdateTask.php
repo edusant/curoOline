3c178b697f1ecd21828c90a85c620bbcc60b60f8
eduardo santos silva <eduardo.silva@kvminformatica.com.br>
2024-08-19 23:07:39 -0300

    Project com as tasks

--- app/Notifications/UpdateTask.php
+++ app/Notifications/UpdateTask.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UpdateTask extends Notification
+class UpdateTask extends Notification implements ShouldQueue
 {
     use Queueable;
 
