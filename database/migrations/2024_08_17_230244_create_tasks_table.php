9bc53d8d7b0751ec776c695747a83d3b5243d1a4
eduardo santos silva <eduardo.silva@kvminformatica.com.br>
2024-08-20 13:04:56 -0300

    Ajustar delete

--- database/migrations/2024_08_17_230244_create_tasks_table.php
+++ database/migrations/2024_08_17_230244_create_tasks_table.php
@@ -18,9 +18,13 @@ return new class extends Migration
             $table->text('descricao');
             $table->date('data_encerramento');
             $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
             $table->unsignedInteger('project_id');
-            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('project_id')->references('id')->on('projects')->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
             $table->timestamps();
         });
     }
